e73be6016cc21a6d7a03691fdcc5e3f9bdda958c
Rigoberto Sanchez <rigobertosanchez1817@gmail.com>
2026-08-16 15:44:55 -0600

    Creacion de prestamos de libros

--- Practicas de POO/Atlantis/src/App.java
+++ Practicas de POO/Atlantis/src/App.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        TanqueOxigeno tanque1 = new TanqueOxigeno(15);
+        TanqueOxigeno tanque2 = new TanqueOxigeno(50);
+
+        Submarino sub1 = new Submarino("Nautilus", tanque1);
+        Submarino sub2 = new Submarino("Poseidon", tanque2);
+
+        List<VehiculoAcuatico> vehiculos = new ArrayList<>();
+        vehiculos.add(sub1);
+        vehiculos.add(sub2);
+
+        for(VehiculoAcuatico v: vehiculos){
+            v.sumergirse();
+        }
+
     }
 }
